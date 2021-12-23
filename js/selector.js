4b61790b2129ba23ea792113b06285f1330b8502
aschimmenti <andschimmenti@gmail.com>
2021-12-23 01:51:29 +0100

    added translations, card, perfect width and height, about page, autocomplete

--- js/selector.js
+++ js/selector.js
@@ -31,7 +31,6 @@ function getSeries2(indexOfTablets) {
   $('#series-list').empty()
   $('#series-index-list').empty()
   $('#series-list').append('<div class="card-deck"><div id="rowdeck" class="row"></div></div>')
-  $('#series-index-list').append('<li style="list-style: none; font-size: larger;">Series / </li>')
   $.each(indexOfTablets, function (key, value) {
     $('#series-index-list').append(`
     <li style="list-style: none;" id="${key}SeriesList"> <a class="cool-link" onclick="addSeriesTablets('${key}')">
@@ -72,7 +71,7 @@ function addSeriesTablets(value) {
   //$(`${value}-tablet-li-elements`).empty()
   $.getJSON('data/index.json', function (indexOfTablets) {
     list_of_codes = indexOfTablets[value]
-    $('.seriesTitle').text(`${value} Series`)
+    $('.seriesTitle').text(`/ ${value} Series /`)
     if ($('#back-btn').length === 0) {
       $('#go-back-button').append(`<a id="back-btn" class="btn px-0 mx-0 my-4" style="font-size:larger"></a>`)
     }
@@ -87,10 +86,10 @@ function addSeriesTablets(value) {
       console.log(value)
       $('#series-description').empty()
       $('#series-description').append(`
-      <div class="text-center">
+      <div class="text-center lead">
         <p>${indexDescriptions[value]['series-description']}</p>
       </div>
-      <div id="descriptions" class="card-deck">
+      <div id="descriptions" class="card-deck gradient-breakline">
       <div class="row">
       
       <div class="col-lg-3 col-md-3 col-sm-6 col d-flex align-items-stretch">
@@ -147,20 +146,29 @@ function addSeriesTablets(value) {
       }
       $(`#${value}-tablet-li-elements`).append(`<li style="list-style: none;"><a class="cool-link" onclick="showTablet('${key}')">${key.replace('_', ' ')}</a></li>`)
       $('#rowdeck-series').append(
-        `<div class="col-lg-12 col-md-12 col-sm-12">
-      <div class="card mb-2 border-0">
-      <div class="card-body text-center">
-      <h5 class="card-title">${key.replace('_', ' ')}</h5>
-      <img class="card-img-top img-responsive tablet-card-size"  style="max-width:50%; max-height:30%" src="data/${key}/${key}.jpg" alt="Card image cap">
-      <p class="card-text"></p>
-      <a onclick="showTablet('${key}')" class="btn btn-outline-secondary">SHOW TABLET</a>
-      </div>
-      </div></div>`
+        `<div class="col-lg-4 col-md-6 col-sm-12">
+        <div class="card border-0 ">
+        <div class="card-body text-center">
+        <img class="card-img-top img-responsive tablet-card-size" src="data/thumbnails/${key}.jpg" alt="Card image cap">
+        <h5 class="card-title">${key.replace('_', ' ')}</h5>
+        <p class="card-text"></p>
+        <a onclick="showTablet('${key}')" class="btn btn-outline-secondary">SHOW TABLET</a>
+        </div>
+        </div></div>`
       )
     });
   })
 }
 
+/*        `<div class="col-lg-4 col-md-6 col-sm-12">
+        <div class="card border-0">
+        <div class="card-body text-center">
+        <img class="card-img-top img-responsive tablet-card-size"  style="max-width:50%; max-height:30%" src="data/thumbnails/${key}.jpg" alt="Card image cap">
+        <h5 class="card-title">${key.replace('_', ' ')}</h5>
+        <p class="card-text"></p>
+        <a onclick="showTablet('${key}')" class="btn btn-outline-secondary">SHOW TABLET</a>
+        </div>
+        </div></div>`*/ 
 
 function iconizeMetadata(input) {
   switch (input) {
@@ -280,7 +288,7 @@ function showTablet(i) {
 
         if (row_n == t[1]) {
           sign = "data/" + i + "/LB_" + i + "_r" + row_n + "_" + t[2] + "_" + t[3] + ".png"
-          row_at_n.push("<figure style='display:inline-block; cursor:pointer;'><img onClick='attestazioni(" + '"' + t[3] + '","' + i + '"' + ")' style='width:50px;' src=" + sign + "><figcaption>" + t[3] + "</figcaption></figure>")
+          row_at_n.push("<figure style='display:inline-block; cursor:pointer;'><img onClick='attestazioni(" + '"' + t[3] + '","' + i + '"' + ")' style='width:50px;' src=" + sign + "><figcaption>" + translateSign(t[3]) + "</figcaption></figure>")
         }
         content.push(row_at_n.join(""))
       });
@@ -302,13 +310,21 @@ function showTablet(i) {
     whiteSource = `data/${i}/${i}_white.jpg`
     $.get(whiteSource)
       .done(function () {
-        tablet = `<a href=${imgSource} target="_blank"><img id="starting-image" style="width:100%;" src="${imgSource}"/></a>
+        tablet = `
+        <div class="row">
+        <div class="col-12">
+        <a href=${imgSource} target="_blank"><img class="img-tablet-final" id="starting-image" src="${imgSource}"/></a>
+      </div>
+      <div class="col-12">
       <button style="background:white;" onClick="pictureChange('${imgSource}')">
         <span aria-hidden="true"><img width="32px" src="images/001-color-circle.png"></span>
       </button>
       <button style="background:white;" onClick="pictureChange('${whiteSource}')">
         <span aria-hidden="true"><img width="32px" src="images/002-color-circle-1.png"></span>
       </button>
+      </div>
+      </div>
+      </div>
     <script>
     function pictureChange(opposite)
     {
@@ -318,8 +334,9 @@ function showTablet(i) {
       `
         $("#tabletShower").append(tablet)
       }).fail(function () {
-        tablet = `<a href=${imgSource} target="_blank"><img style="width:100%;" src="${imgSource}"/></a>`
+        tablet = `<a href=${imgSource} target="_blank"><img class="img-tablet-final" src="${imgSource}"/></a>`
         $("#tabletShower").append(tablet)
+        $(window).scrollTop(0);
       })
   });
 }
@@ -383,6 +400,195 @@ function showAttestazioni(sign, filemap) {
   })
   console.log(content)
   $('#attestazioni').append(content.join(''))
-  $('#attestazioni').prepend('<h5>Tablets with ' + sign + '</h5>')
+  $('#attestazioni').prepend('<h5>Attestations of ' + sign + '</h5>')
   document.getElementById('#attestazioni').scrollIntoView();
+}
+
+
+
+function translateSign(sign) {
+  linearB = {
+    "zo" : "20",
+    "ze" : "74",
+    "za" : "17",
+    "z" : "110",
+    "wo" : "42",
+    "wi" : "40",
+    "we" : "75",
+    "wa" : "54",
+    "vir" : "100",
+    "vin" : "131",
+    "v" : "111",
+    "u" : "10",
+    "two" : "91",
+    "turo2" : "156",
+    "tu" : "69",
+    "to" : "05",
+    "ti" : "37",
+    "tela+te" : "159+te",
+    "tela+pu" : "159+pu",
+    "tela+pa" : "159+pa",
+    "tela" : "159",
+    "te" : "04",
+    "ta2" : "66",
+    "ta" : "59",
+    "t" : "112",
+    "susf" : "108f",
+    "susm" : "108m",
+    "sus+si" : "108+si",
+    "sus+ka" : "108+ka",
+    "sus" : "108",
+    "su" : "58",
+    "so" : "12",
+    "si" : "41",
+    "se" : "09",
+    "sa" : "31",
+    "s" : "113",
+    "ru" : "26",
+    "rota+te" : "243+te",
+    "rota" : "243",
+    "ro2" : "68",
+    "ro" : "02",
+    "ri" : "53",
+    "re" : "27",
+    "ra3" : "33",
+    "ra2" : "76",
+    "ra" : "60",
+    "qo" : "32",
+    "qi" : "21",
+    "qe" : "78",
+    "qa" : "16",
+    "pu2" : "29",
+    "pu" : "50",
+    "pte" : "62",
+    "po" : "11",
+    "pi" : "39",
+    "pe" : "72",
+    "pa" : "03",
+    "p" : "115",
+    "ovism" : "106m",
+    "ovisf" : "106f",
+    "ovis+ta" : "106+ta",
+    "ovis" : "106",
+    "olo+we" : "130+we",
+    "oliv" : "122",
+    "ole+po" : "130+po",
+    "ole+pa" : "130+pa",
+    "ole+a" : "130+a",
+    "ole" : "130",
+    "o" : "61",
+    "nwa" : "48",
+    "nu" : "55",
+    "no" : "52",
+    "ni" : "30",
+    "ne" : "24",
+    "na" : "06",
+    "n" : "116",
+    "mul" : "102",
+    "mu" : "23",
+    "mo" : "15",
+    "mi" : "73",
+    "me" : "13",
+    "ma" : "80",
+    "m" : "117",
+    "lana" : "145",
+    "l" : "118",
+    "ku" : "81",
+    "ko" : "70",
+    "ki" : "67",
+    "ke" : "44",
+    "kapo" : "127",
+    "ka" : "77",
+    "jo" : "36",
+    "je" : "46",
+    "ja" : "57",
+    "i" : "28",
+    "hord" : "121",
+    "gra" : "120",    
+    "far" : "129",
+    "equ" : "105",
+    "e" : "38",
+    "dwo" : "90",
+    "dwe" : "71",
+    "du" : "51",
+    "do" : "14",
+    "di" : "07",
+    "de" : "45",
+    "da" : "01",
+    "cyp+pa" : "125+pa",
+    "cyp+o" : "125+o",
+    "cerv" : "104",
+    "capm" : "107m",
+    "capf" : "107f",
+    "cap" : "107",
+    "bosm" : "109m",
+    "bosf" : "109f",
+    "bos+si" : "109+si",
+    "bos" : "109m",
+    "aur" : "141/a",
+    "au" : "85",
+    "arom" : "123",
+    "arm" : "163",
+    "arg" : "141/b",
+    "arepa" : "133",
+    "alv" : "225",
+    "aes" : "140",
+    "a3" : "43",
+    "a2" : "25",
+    "a" : "08",
+    "234" : "243",
+    "86" : "86",
+    "83" : "83",
+    "82" : "82",
+    "79" : "79",
+    "65" : "65",
+    "64" : "64",
+    "63" : "63",
+    "56" : "56",
+    "34" : "34",
+    "19" : "19",
+    "251" : "251",
+    "250vas" : "250vas",
+    "249" : "249",
+    "232" : "232",
+    "228vas" : "228vas",
+    "220" : "220",
+    "219vas" : "219vas",
+    "216vas" : "216vas",
+    "215vas" : "215vas",
+    "214vas" : "214vas",
+    "213vas" : "213vas",
+    "212vas" : "212vas",
+    "209vas" : "209vas",
+    "208vas" : "208vas",
+    "206vas" : "206vas",
+    "205vas" : "205vas",
+    "204vas" : "204vas",
+    "203vas" : "203vas",
+    "202vas" : "202vas",
+    "201vas" : "201vas",
+    "200vas" : "200vas",
+    "189" : "189",
+    "171" : "171",
+    "169" : "169",
+    "166+we" : "166+we",
+    "157" : "157",
+    "154" : "154",
+    "153" : "153",
+    "152" : "152",
+    "146" : "146",
+    "132" : "132"
+}
+
+translation = []
+$.each(linearB, function(letter, code) {
+  if (sign == code) {
+    translation.push(letter)
+  } 
+  else if (sign.includes("numeral")) {
+    x = sign.replace("numeral","");
+    translation.push(x)
+  }
+})
+return translation[0]
 }
